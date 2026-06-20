5dcd5d90cca158ec28b795c94ec00b39b3255fa4
brayan <brayanstivenp578@gmail.com>
2026-06-20 17:54:35 -0500

    se agrega dependencia swagger

---
+++ app-microservice-cliente-app/src/main/java/com/brayanpv/app/infrastructure/config/SpringDocConfig.java
@@ -0,0 +1,24 @@
+package com.brayanpv.app.infrastructure.config;
+
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocConfig {
+
+    @Bean
+    public OpenAPI clienteServiceOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Cliente Service API")
+                        .description("Microservicio de gestión de clientes - Prueba técnica Devsu")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("Brayan")
+                                .url("https://github.com/zangxs/api-devsu-technical")));
+    }
+}
